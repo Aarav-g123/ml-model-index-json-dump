c197c2cca5698b7df2684b24f7b96eee0db91283
Aarav-g123 <138991532+Aarav-g123@users.noreply.github.com>
2023-08-25 00:18:18 +0100

    Made the dump script

---
+++ dumpscript.py
@@ -0,0 +1,19 @@
+import json
+from safetensors import safe_open
+
+# Load the safetensors file
+with safe_open("path/to/model.safetensors", framework="pt", device="cpu") as f:
+    tensors = f.get_tensors()
+
+# Create the model_index.json file
+model_index = {
+    "model": "Neonmcgameromgomg/chilloutmix",
+    "files": {
+        "safetensors": "path/to/model.safetensors"
+    },
+    "tensors": tensors
+}
+
+# Save the model_index.json file
+with open("path/to/model_index.json", "w") as f:
+    json.dump(model_index, f)
